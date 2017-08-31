c11bba053307df5048905a83c6cfc499a25c9334
vegaboyster <31493874+vegaboyster@users.noreply.github.com>
2017-08-31 01:42:52 +0100

    Update cpum.sh

--- cpum.sh
+++ cpum.sh
@@ -16,6 +16,7 @@ sed $'s/\r$//' tempconf.txt >config.txt
 rm tempconf.txt
 read -p "Enter worker name: " workername
 workername=${workername}
+echo $workername
 sed -i 's/"pool_address" : "pool.usxmrpool.com:3333",/"pool_address" : "pool.supportxmr.com:7777",/g' config.txt
 sed -i 's/"wallet_address" : "",/"wallet_address" : "44QM2gXjRhFfLjFTZMGyc5CzGo61oznL1ZoLdfihwAqPPDk8Kp2jRxo6zQzTqh3Q1N2ytk5SHEpGfRZ9XwrV78hoBfFSahS",/g' config.txt
 sed -i 's/"pool_password" : "",/"pool_password" : "'$workername':vegaboyster@gmail.com",/g' config.txt

93fe1639795c659a381f42c118efb79d99a50bfd
vegaboyster <31493874+vegaboyster@users.noreply.github.com>
2018-02-01 22:44:05 +0000

    Update cpum.sh

--- cpum.sh
+++ cpum.sh
@@ -2,22 +2,13 @@
 apt -y update
 apt -y upgrade
 apt -y install mc libmicrohttpd-dev libssl-dev cmake build-essential git screen libhwloc-dev
-git clone https://github.com/fireice-uk/xmr-stak-cpu.git
+git clone https://github.com/fireice-uk/xmr-stak.git
 sysctl -w vm.nr_hugepages=128
 echo "* soft memlock 262144" >> /etc/security/limits.conf
 echo "* hard memlock 262144" >> /etc/security/limits.conf
-cd xmr-stak-cpu
-cmake .
+cd xmr-stak
+cmake . -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make
-cp bin/xmr-stak-cpu /usr/local/bin
-cp config.txt ~/tempconf.txt
+cp bin/xmr-stak /usr/local/bin
 cd ~
-sed $'s/\r$//' tempconf.txt >config.txt
-rm tempconf.txt
-read -p "Enter worker name: " workername
-workername=${workername}
-sed -i 's/"pool_address" : "pool.usxmrpool.com:3333",/"pool_address" : "pool.supportxmr.com:7777",/g' config.txt
-sed -i 's/"wallet_address" : "",/"wallet_address" : "44QM2gXjRhFfLjFTZMGyc5CzGo61oznL1ZoLdfihwAqPPDk8Kp2jRxo6zQzTqh3Q1N2ytk5SHEpGfRZ9XwrV78hoBfFSahS",/g' config.txt
-sed -i 's/"pool_password" : "",/"pool_password" : "'$workername':vegaboyster@gmail.com",/g' config.txt
-sed -i 's/"verbose_level" : 3,/"verbose_level" : 4,/g' config.txt
-rm -Rf xmr-stak-cpu cpum.sh
+rm -Rf xmr-stak cpum.sh

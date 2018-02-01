3cdb42ee0cc5daafca9357e81dc4f444b7008e06
vegaboyster <31493874+vegaboyster@users.noreply.github.com>
2018-02-01 22:22:29 +0000

    Update cpumf.sh

--- cpumf.sh
+++ cpumf.sh
@@ -9,14 +9,14 @@ cd xmr-stak-cpu
 cmake .
 make
 cp bin/xmr-stak-cpu /usr/local/bin
-cp config.txt ~/tempconf.txt
+#cp config.txt ~/tempconf.txt
 cd ~
-sed $'s/\r$//' tempconf.txt >config.txt
-rm tempconf.txt
-read -p "Enter worker name: " workername
-workername=${workername}
-sed -i 's/"pool_address" : "pool.usxmrpool.com:3333",/"pool_address" : "pool.supportxmr.com:7777",/g' config.txt
-sed -i 's/"wallet_address" : "",/"wallet_address" : "44QM2gXjRhFfLjFTZMGyc5CzGo61oznL1ZoLdfihwAqPPDk8Kp2jRxo6zQzTqh3Q1N2ytk5SHEpGfRZ9XwrV78hoBfFSahS",/g' config.txt
-sed -i 's/"pool_password" : "",/"pool_password" : "'$workername':vegaboyster@gmail.com",/g' config.txt
-sed -i 's/"verbose_level" : 3,/"verbose_level" : 4,/g' config.txt
+#sed $'s/\r$//' tempconf.txt >config.txt
+#rm tempconf.txt
+#read -p "Enter worker name: " workername
+#workername=${workername}
+#sed -i 's/"pool_address" : "pool.usxmrpool.com:3333",/"pool_address" : "pool.supportxmr.com:7777",/g' config.txt
+#sed -i 's/"wallet_address" : "",/"wallet_address" : "44QM2gXjRhFfLjFTZMGyc5CzGo61oznL1ZoLdfihwAqPPDk8Kp2jRxo6zQzTqh3Q1N2ytk5SHEpGfRZ9XwrV78hoBfFSahS",/g' config.txt
+#sed -i 's/"pool_password" : "",/"pool_password" : "'$workername':vegaboyster@gmail.com",/g' config.txt
+#sed -i 's/"verbose_level" : 3,/"verbose_level" : 4,/g' config.txt
 rm -Rf xmr-stak-cpu cpumf.sh

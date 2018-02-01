1d7d479c129932cd2eae2413aeaf1dcf673768d9
vegaboyster <31493874+vegaboyster@users.noreply.github.com>
2018-02-01 22:20:44 +0000

    Update cpumf.sh

--- cpumf.sh
+++ cpumf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dnf -y update
 dnf -y install mc gcc gcc-c++ cmake hwloc-devel libmicrohttpd-devel openssl-devel rapidjson-devel git screen
-git clone https://github.com/fireice-uk/xmr-stak-cpu.git
+git clone https://github.com/fireice-uk/xmr-stak.git
 sysctl -w vm.nr_hugepages=128
 echo "* soft memlock 262144" >> /etc/security/limits.conf
 echo "* hard memlock 262144" >> /etc/security/limits.conf
